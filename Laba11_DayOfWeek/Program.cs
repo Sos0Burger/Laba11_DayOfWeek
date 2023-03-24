69b79ae8b8a6c677082d23a5c8a6f9a43966b3e8
Ixor <dsv>
2023-03-24 10:02:20 +0300

    Добавьте файлы проекта.

---
+++ Laba11_DayOfWeek/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    static int dayNumber = 0;
+    private static void Main(string[] args)
+    {
+        DayOfWeek dof = () =>
+        {
+
+            Program.dayNumber++;
+            switch (Program.dayNumber)
+            {
+                case 1:
+                    return "Понедельник";
+                case 2:
+                    return "Вторник";
+                case 3:
+                    return "Среда";
+                case 4:
+                    return "Четверг";
+                case 5:
+                    return "Пятница";
+                case 6:
+                    return "Суббота";
+                case 7:
+                    return "Воскресенье";
+                default:
+                    Program.dayNumber = 1;
+                    return "Понедельник";
+            }
+        };
+        for (int i = 0; i < 14; i++)
+        {
+            Console.WriteLine(dof());
+        }
+
+    }
+}
+delegate string DayOfWeek();
